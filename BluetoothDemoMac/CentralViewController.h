f924b06fed971c8ce348fa48ba01b9d7be368c82
tomfriwel <976932447@qq.com>
2018-05-04 16:09:50 +0800

    handle property

---
+++ BluetoothDemoMac/CentralViewController.h
@@ -0,0 +1,13 @@
+//
+//  CentralViewController.h
+//  BluetoothDemoMac
+//
+//  Created by tomfriwel on 2018/5/4.
+//  Copyright © 2018 tomfriwel. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CentralViewController : NSViewController
+
+@end
---
+++ BluetoothDemoMac/CentralViewController.m
@@ -0,0 +1,13 @@
+//
+//  CentralViewController.m
+//  BluetoothDemoMac
+//
+//  Created by tomfriwel on 2018/5/4.
+//  Copyright © 2018 tomfriwel. All rights reserved.
+//
+
+#import "CentralViewController.h"
+
+@implementation CentralViewController
+
+@end
